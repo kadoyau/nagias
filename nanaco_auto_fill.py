4f7004f1bda11746a27ce959fa6b0a5cd10588f5
kadoyau <el.psi.congroo@gmail.com>
2017-07-03 01:43:43 +0900

    first commit

---
+++ nanaco_auto_fill.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+ 
+# Chrome起動時のオプションの設定
+options = webdriver.ChromeOptions()
+#options.binary_location ='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+# 自分の環境だとCanaryでないと動かなかった
+options.binary_location = '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
+options.add_argument('headless')
+options.add_argument('window-size=1200x600')
+driver = webdriver.Chrome(chrome_options=options)
+
+# nanacoのログインページへアクセス
+driver.get('https://www.nanaco-net.jp/pc/emServlet')
+
+# wait up to 10 seconds for the elements to become available
+driver.implicitly_wait(10)
+
+# nanacoのサイトにログインする
+email = driver.find_element_by_css_selector('#loginByPassword input[type=text]')
+password = driver.find_element_by_css_selector('#loginByPassword input[type=password]')
+with open('.secret') as f:
+    credentials = f.read().strip().split('\t')
+
+email.send_keys(credentials[0])
+password.send_keys(credentials[1])
+# @see http://selenium-python.readthedocs.io/api.html#module-selenium.webdriver.common.keys
+password.send_keys(Keys.RETURN)
