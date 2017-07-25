20f95494480e7e0919c05276e40c2f065195c85d
kadoyau <el.psi.congroo@gmail.com>
2017-07-26 03:54:35 +0900

    ギフトコードの入力をメソッドに切り出す

--- nanaco_auto_fill.py
+++ nanaco_auto_fill.py
@@ -13,6 +13,7 @@ class NanacoAutoFiller:
 
     def __init__(self, use_canary = False):
         self.__use_canary = use_canary
+        self.__driver = self.__set_driver()
     
     def __set_driver(self):
         '''Chrome起動時のオプションの設定'''
@@ -25,19 +26,27 @@ class NanacoAutoFiller:
         options.add_argument('window-size=1200x600')
         return webdriver.Chrome(chrome_options=options)
 
+    def __input_codes(self,code):
+        '''ギフトコードを入力する'''
+        SPLIT_LENGTH = 4
+        SPLITED_CODES = [code[i: i + SPLIT_LENGTH] for i in range(0, len(code), SPLIT_LENGTH)]
+        # コードを入力する
+        self.__driver.find_element_by_id('gift01').send_keys(SPLITED_CODES[0])
+        self.__driver.find_element_by_id('gift02').send_keys(SPLITED_CODES[1])
+        self.__driver.find_element_by_id('gift03').send_keys(SPLITED_CODES[2])
+        self.__driver.find_element_by_id('gift04').send_keys(SPLITED_CODES[3])
+
     def main(self):
         # TODO: usageを書く
-        DRIVER = self.__set_driver()
+        # タイムアウトまでのデフォルト秒数を指定する
+        self.__driver.implicitly_wait(3)
 
         # nanacoのログインページへアクセス
-        DRIVER.get('https://www.nanaco-net.jp/pc/emServlet')
-
-        # タイムアウトまでのデフォルト秒数を指定する
-        DRIVER.implicitly_wait(3)
+        self.__driver.get('https://www.nanaco-net.jp/pc/emServlet')
 
         # nanacoのサイトにログインする
-        EMAIL = DRIVER.find_element_by_css_selector('#loginByPassword input[type=text]')
-        PASSWORD = DRIVER.find_element_by_css_selector('#loginByPassword input[type=password]')
+        EMAIL = self.__driver.find_element_by_css_selector('#loginByPassword input[type=text]')
+        PASSWORD = self.__driver.find_element_by_css_selector('#loginByPassword input[type=password]')
         with open('.secret') as f:
             CREDENTIALS = f.read().strip().split('\t')
 
@@ -47,8 +56,7 @@ class NanacoAutoFiller:
         PASSWORD.send_keys(Keys.RETURN)
 
         # ギフト登録約款ページに飛ぶ
-        DRIVER.find_element_by_css_selector('#memberNavi02').click()
-
+        self.__driver.find_element_by_css_selector('#memberNavi02').click()
 
         # コードの数だけ入力を繰り返す
         # コードを全て取得する
@@ -57,42 +65,36 @@ class NanacoAutoFiller:
 
         for code in CODES:
             # ギフト登録ページにジャンプする
-            DRIVER.find_element_by_css_selector('#register input[type=image]').click()
+            self.__driver.find_element_by_css_selector('#register input[type=image]').click()
 
             # ギフト登録ページのウィンドウに制御を移す
             # @see http://qiita.com/QUANON/items/285ad7157619b0da5c67
-            main_page =  DRIVER.window_handles[0]
+            main_page =  self.__driver.window_handles[0]
 
             # 別ウィンドウのハンドラを取得する
-            WebDriverWait(DRIVER, 3).until(lambda d: len(d.window_handles) > 1)
-            gift_page_handle =  DRIVER.window_handles[1]
-            DRIVER.switch_to.window(gift_page_handle)
+            WebDriverWait(self.__driver, 3).until(lambda d: len(d.window_handles) > 1)
+            gift_page_handle =  self.__driver.window_handles[1]
+            self.__driver.switch_to.window(gift_page_handle)
 
-            SPLIT_LENGTH = 4
-            splited_code = [code[i: i + SPLIT_LENGTH] for i in range(0, len(code), SPLIT_LENGTH)]
-            # コードを入力する
-            DRIVER.find_element_by_id('gift01').send_keys(splited_code[0])
-            DRIVER.find_element_by_id('gift02').send_keys(splited_code[1])
-            DRIVER.find_element_by_id('gift03').send_keys(splited_code[2])
-            DRIVER.find_element_by_id('gift04').send_keys(splited_code[3])
+            self.__input_codes(code)
 
-            DRIVER.find_element_by_id('submit-button').click()
+            self.__driver.find_element_by_id('submit-button').click()
 
             # 登録するボタンを押す
             try:
-                DRIVER.find_element_by_css_selector('#nav2Next input[type=image]').click()
+                self.__driver.find_element_by_css_selector('#nav2Next input[type=image]').click()
                 self.__results["success"].append(code)
             except NoSuchElementException:
                 self.__results["fairule"].append(code)
 
             # 当該ウィンドウを終了する
             # @see https://stackoverflow.com/questions/35286094/how-to-close-the-whole-browser-window-by-keeping-the-webdriver-active
-            DRIVER.close()
+            self.__driver.close()
 
             # はじめのウィンドウに戻る
-            DRIVER.switch_to.window(main_page)
+            self.__driver.switch_to.window(main_page)
 
-        DRIVER.quit()
+        self.__driver.quit()
 
     def output(self):
         """結果を表示する"""
